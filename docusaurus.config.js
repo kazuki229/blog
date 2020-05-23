183c0469bce2166a8e464d968ebb8d70b5202f4b
kazuki229 <tsuzuku.k@gmail.com>
2020-05-23 14:47:00 +0900

    :tada: Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,54 @@
+module.exports = {
+  title: 'kazuki229\'s blog',
+  url: 'https://kazuki229.github.io',
+  baseUrl: '/',
+  favicon: 'img/logo.png',
+  organizationName: 'kazuki229',
+  projectName: 'blog',
+  themeConfig: {
+    navbar: {
+      title: 'kazuki229',
+      logo: {
+        alt: 'kazuki229 logo',
+        src: 'img/logo.png',
+      },
+      links: [
+        {
+          href: 'https://twitter.com/kazuki229_dev',
+          label: 'Twitter',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/kazuki229',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} kazuki229. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        blog: {
+          showReadingTime: true,
+          // TODO
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+          feedOptions: {
+            type: 'all',
+            copyright: `Copyright © ${new Date().getFullYear()} kazuki229. Built with Docusaurus.`,
+          },
+          routeBasePath: '/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
