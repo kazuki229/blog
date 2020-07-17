52c35e27efce62d0b0737c7d6f058d4feaef90a5
kazuki229 <tsuzuku.k@gmail.com>
2020-07-18 00:04:29 +0900

    :microscope: Add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: process.env.GA_TRACKING_ID,
+        head: true,
       },
     },
     `gatsby-plugin-feed`,
