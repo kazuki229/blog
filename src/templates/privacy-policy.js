94c3088b08d794d0547152699fe95a441643a75c
kazuki229 <tsuzuku.k@gmail.com>
2020-07-26 00:55:06 +0900

    :art: Design privacy policy

--- src/templates/privacy-policy.js
+++ src/templates/privacy-policy.js
@@ -1,42 +1,23 @@
 import React from "react"
-import PropTypes from "prop-types"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 // Components
-const PrivacyPolicy = ({ pageContext, data }) => {
+const PrivacyPolicy = () => {
     return (
-        <div>
-            <h1>プライバシーポリシー</h1>
-            <h2>当サイトが使用しているアクセス解析ツールについて</h2>
-            当サイトでは、Googleによるアクセス解析ツール「Googleアナリティクス」を利用しています。
-            このGoogleアナリティクスはトラフィックデータの収集のためにCookieを使用しています。
-            このトラフィックデータは匿名で収集されており、個人を特定するものではありません。
-            この機能はCookieを無効にすることで収集を拒否することが出来ますので、お使いのブラウザの設定をご確認ください。
-            この規約に関して、詳しくは<a href="https://marketingplatform.google.com/about/analytics/terms/jp/">こちら</a>、または<a href="https://policies.google.com/technologies/partner-sites?hl=ja">こちら</a>をクリックしてください。
-        </div>
+        <Layout location="privacy-policy" title="プライバシーポリシー">
+            <SEO title="プライバシーポリシー" />
+            <div>
+                <h1>プライバシーポリシー</h1>
+                <h2>当サイトが使用しているアクセス解析ツールについて</h2>
+                <p>当サイトでは、Googleによるアクセス解析ツール「Googleアナリティクス」を利用しています。</p>
+                <p>このGoogleアナリティクスはトラフィックデータの収集のためにCookieを使用しています。</p>
+                <p>このトラフィックデータは匿名で収集されており、個人を特定するものではありません。</p>
+                <p>この機能はCookieを無効にすることで収集を拒否することが出来ますので、お使いのブラウザの設定をご確認ください。</p>
+                <p>この規約に関して、詳しくは<a href="https://marketingplatform.google.com/about/analytics/terms/jp/">こちら</a>、または<a href="https://policies.google.com/technologies/partner-sites?hl=ja">こちら</a>をクリックしてください。</p>
+            </div>
+        </Layout >
     )
-}
-
-PrivacyPolicy.propTypes = {
-    pageContext: PropTypes.shape({
-        tag: PropTypes.string.isRequired,
-    }),
-    data: PropTypes.shape({
-        allMarkdownRemark: PropTypes.shape({
-            totalCount: PropTypes.number.isRequired,
-            edges: PropTypes.arrayOf(
-                PropTypes.shape({
-                    node: PropTypes.shape({
-                        frontmatter: PropTypes.shape({
-                            title: PropTypes.string.isRequired,
-                        }),
-                        fields: PropTypes.shape({
-                            slug: PropTypes.string.isRequired,
-                        }),
-                    }),
-                }).isRequired
-            ),
-        }),
-    }),
 }
 
 export default PrivacyPolicy
