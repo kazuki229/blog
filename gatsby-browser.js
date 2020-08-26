391512265a633acb9955e71620f5df2439d706b6
kazuki229 <tsuzuku.k@gmail.com>
2020-08-26 23:47:54 +0900

    :art: Add code hilight

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,5 @@ import "typeface-montserrat"
 import "typeface-merriweather"
 
 import "prismjs/themes/prism.css"
+import 'gatsby-prismjs-dracula'
+require("prismjs/plugins/line-numbers/prism-line-numbers.css")
